4a0ebac0cee2d117d3e809086c0d672b781b6ba6
gboothroyd <89821743+gboothroyd@users.noreply.github.com>
2021-09-29 19:54:52 -0500

    Week 5 Assessment
    My Week 5 Assessment using html files to add to my Movie database

---
+++ src/main/java/AddItemServlet.java
@@ -0,0 +1,37 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AddItemServlet
+ */
+@WebServlet("/addItemServlet")
+public class AddItemServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AddItemServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String movie = request.getParameter("movie");
+		String genre = request.getParameter("genre");
+		Movie m = new Movie(movie, genre);
+		MovieHelper mh = new MovieHelper();
+		mh.insertItem(m);
+		getServletContext().getRequestDispatcher("/movieindex.html").forward(request, response);
+	}
+
+}
